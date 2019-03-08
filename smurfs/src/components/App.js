55cded884b64d6335fe8c1ae83186b79b9d2295b
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-08 12:04:45 +0100

    Add comments

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,30 +4,38 @@ import { connect } from 'react-redux';
 import { getSmurfs, addSmurf, deleteSmurf } from '../actions';
 import './App.css';
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 export class App extends React.Component {
+  //refs to handle adding smurfs
   nameRef = React.createRef();
   ageRef = React.createRef();
   heightRef = React.createRef();
+
+  clearInput = () => {
+    this.nameRef.current.value = '';
+    this.ageRef.current.value = '';
+    this.heightRef.current.value = '';
+  };
 
   render() {
     return (
       <div className="App">
         <h1>SMURFS!</h1>
 
+        {/* button to load smurfs - on click loads smurfs*/}
         <button
           onClick={() => {
             this.props.getSmurfs();
           }}>
           Show me some smurfs!
         </button>
-        <div>{this.props.fetchingSmurfs && <div>Fetching...</div>}</div>
-        <div>{this.props.error && <div>{this.props.error}</div>}</div>
+
+        {/* div for handling error and showing fetching */}
+        <div>
+          {this.props.fetchingSmurfs && <div>Fetching...</div>}
+          {this.props.error && <div>{this.props.error}</div>}
+        </div>
+
+        {/* form for adding smurfs - on click adds smurf */}
         <form>
           <h4>Add some smurfs</h4>
           <div>
@@ -47,26 +55,32 @@ export class App extends React.Component {
             onClick={event => {
               event.preventDefault();
               this.props.addSmurf(this.nameRef.current.value, this.ageRef.current.value, this.heightRef.current.value);
+              this.clearInput();
             }}>
             Submit
           </button>
         </form>
-        <div>{this.props.addingSmurfs && <div>Adding...</div>}</div>
-        <div>{this.props.deletingSmurfs && <div>Deleting...</div>}</div>
 
+        {/* div for handling showing adding and deleting */}
+        <div>
+          {this.props.addingSmurfs && <div>Adding...</div>}
+          {this.props.deletingSmurfs && <div>Deleting...</div>}
+        </div>
+
+        {/* div for smurf displaying */}
         <div>
           {this.props.smurfs &&
             this.props.smurfs.map(smurf => {
               return (
                 <div key={smurf.name}>
                   <div>
-                    Name:<span>{smurf.name}</span>
+                    Name: <span>{smurf.name}</span>
                   </div>
                   <div>
-                    Age:<span>{smurf.age}</span>
+                    Age: <span>{smurf.age}</span>
                   </div>
                   <div>
-                    Height:<span>{smurf.height}</span>
+                    Height: <span>{smurf.height}</span>
                   </div>
                   <button
                     onClick={event => {
@@ -84,6 +98,7 @@ export class App extends React.Component {
   }
 }
 
+//redux state
 function mapStateToProps(state) {
   return {
     smurfs: state.smurfs,
